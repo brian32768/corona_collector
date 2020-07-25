e9edc74150ec7518e539cab2d76a4975bc722ed0
Brian H Wilson <brian@wildsong.biz>
2020-07-24 19:56:39 -0700

    overwrite dont delete

--- generator.py
+++ generator.py
@@ -79,14 +79,29 @@ def metrocases():
     return cases('Metro', df)
 
 
+def recursive_overwrite(src, dest, ignore=None):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = os.listdir(src)
+        if ignore is not None:
+            ignored = ignore(src, files)
+        else:
+            ignored = set()
+        for f in files:
+            if f not in ignored:
+                recursive_overwrite(os.path.join(src, f),
+                                    os.path.join(dest, f),
+                                    ignore)
+    else:
+        shutil.copyfile(src, dest)
 
 if __name__ == "__main__":
 
     # Wipe output and
     # Copy static content to output. 
     try:
-        shutil.rmtree('public', ignore_errors=True)
-        shutil.copytree('static', 'public/static')
+        recursive_overwrite('static', 'public/static')
     except Exception as e:
         print("Error copying static files. %s" % e)
 
