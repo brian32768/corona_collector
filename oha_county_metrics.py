35cb3af68a87d94c1afbe27b10ec97276f76c632
bwilsoncc <bwilsoncc@co.clatsop.or.us>
2020-12-14 10:35:10 -0800

    metrics update

--- oha_county_metrics.py
+++ oha_county_metrics.py
@@ -4,7 +4,7 @@ import csv
 import jinja2
 from datetime import datetime
 
-date_range = "Nov 1 - Nov 28, 2020"
+date_range = "Nov 8 - Dec 5, 2020"
 
 TEMPLATE_DIR = 'templates'
 DATA_FILE = 'data/oha_county_metrics.csv'
